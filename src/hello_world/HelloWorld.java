404c19fa857ccfc6cd1d424c906a991c82ad6b00
z2140091 <z2140091@192.168.128.106>
2017-07-26 16:01:51 +0900

    英語から日本語に修正

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -3,7 +3,7 @@ package hello_world;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Wolrd");
+		System.out.println("Hello 世界！");
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
